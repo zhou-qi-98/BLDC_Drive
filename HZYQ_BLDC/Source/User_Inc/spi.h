577c898e8946a97a3d0f8ace0061fc4cea8d458d
zhouqi <admin@7yunkj.com>
2023-03-25 10:44:22 +0800

    更新项目

--- HZYQ_BLDC/Source/User_Inc/spi.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SPI_H
-#define __SPI_H
-
-
-#include "typedef.h"
-
-void Spi_SendData(void);
-
-#endif
--- HZYQ_BLDC/project/Objects/bsp.d
+++
@@ -1,7 +0,0 @@
-.\objects\bsp.o: ..\Source\MyDrivers\bsp.c
-.\objects\bsp.o: ..\Source\User_Inc\bsp.h
-.\objects\bsp.o: ..\Source\User_Inc\gpio.h
-.\objects\bsp.o: ..\Source\User_Inc\typedef.h
-.\objects\bsp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\bsp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\bsp.o: ..\Source\User_Inc\var.h
